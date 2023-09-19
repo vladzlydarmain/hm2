10422b4de1bc7d9f418cc79e2f37ee10bc67cd2e
Vlad <vladzlydar2008@gmail.com>
2023-09-19 13:53:42 +0300

    first

---
+++ index.js
@@ -0,0 +1,6 @@
+let name = "Vlad"
+let surname = "Zlydar"
+let age = 14
+let hobby = "грати з друзями"
+
+console.log(`Мене звати ${name} ${surname} мені ${age} років, у вільний час я полюбляю ${hobby}`)
