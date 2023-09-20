249c3d9509639825cf2dbe6091ae58ffe6575432
Vlad <vladzlydar2008@gmail.com>
2023-09-20 19:32:53 +0300

    modified:   index.js

--- index.js
+++ index.js
@@ -4,22 +4,10 @@ let rl = readline.createInterface({
     output:process.stdout
 })
 
-rl.question("Enter coefficient - a ", (a) => {
-    rl.question("Enter coefficient - b ", (b) => {
-        rl.question("Enter coefficient - c ", (c) => {
-            a = parseFloat(a);
-            b = parseFloat(b);
-            c = parseFloat(c);
-            let d = b**2 - 4*a*c;
-            if (d < 0){
-                console.log("Коренів немає")
-            } else if(d == 0){
-                console.log(-b/(2*a))
-            } else if(d > 0){
-                let x1 = (-b-Math.sqrt(d))/(2*a)
-                let x2 = (-b+Math.sqrt(d))/(2*a)
-                console.log(`Перший корінь = ${x1}, другий корінь = ${x2}`)
-            }
-        })
+rl.question("Введіть довжину: ", (a) => {
+    rl.question("Введіть ширину: ", (b) => {
+        a = parseFloat(a)
+        b = parseFloat(b)
+        console.log(`P = ${2*(a+b)}`)
     })
-}) 
+})
